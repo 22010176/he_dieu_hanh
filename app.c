fd41f8e2df93e85c709c94e57f1f6d2c18ba60c5
Do Duc Minh <22010176@st.phenikaa-uni.edu.vn>
2024-01-03 21:57:56 +0700

    ed

--- app.c
+++ app.c
@@ -4,35 +4,46 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-uint8_t flag[2] = { 0,0 };
-uint32_t turn = 1;
+int flags[2] = { 0,0 };             // { 0,0 }, 0
+int turn = 0;
+int time = 0;
 
 void P0() {
-    for (;;) {
-        flag[0] = turn = 1;
-        while (flag[1] && turn); // Lap cho den khi dieu kien ko thoa man
-        // doan nguy hiem
-        flag[0] = 0;
-        // phan con lai
-    }
+  flags[0] = turn = 1;             // { 1,0 }, 1 : p0 vao khu vuc nguy hiem, luot p1
+  printf("%d. f0: %d f1: %d t: %d\n", ++time, flags[0], flags[1], turn);
+  // Neu p1 dang hoat dong va p1 dang tiep can khu vuc nguy hiem.
+  // Lap cho den khi dieu kien ko thoa man | (0 && 1) = 0
+  while (flags[1] && turn) printf("%d. f0: %d f1: %d t: %d\n", ++time, flags[0], flags[1], turn);
+
+  // doan nguy hiem
+  flags[0] = 0;                    // { 0,0 }, 1 : p0 thoat khu vuc nguy hiem, luot p1
+  printf("%d. f0: %d f1: %d t: %d\n", ++time, flags[0], flags[1], turn);
+  // phan con lai
 }
 void P1() {
-    for (;;) {
-        flag[1] = 1, turn = 0;
-        while (flag[0] && !turn); // Lap cho den khi dieu kien ko thoa man
-        // doan nguy hiem
-        flag[1] = 0;
-        // phan con lai
-    }
+  flags[1] = 1, turn = 0;          // { 0,1 }, 0 : p1 vao khu vuc nguy hiem, luot p0
+  printf("%d. f0: %d f1: %d t: %d\n", ++time, flags[0], flags[1], turn);
+
+  // Neu p0 dang hoat dong va p0 dang tiep can khu vuc nguy hiem.  
+  // Lap cho den khi dieu kien ko thoa man | (0 && !0) == 0;
+  while (flags[0] && !turn) printf("%d. f0: %d f1: %d t: %d\n", ++time, flags[0], flags[1], turn);
+
+  // doan nguy hiem
+  flags[1] = 0;                    // { 0,0 }, 0 : p1 thoat khu vuc nguy hiem, luot p0
+  printf("%d. f0: %d f1: %d t: %d\n", ++time, flags[0], flags[1], turn);
+  // phan con lai
+  printf("\n\n");
 }
 
 int main() {
-    flag[0] = flag[1] = turn = 0;
+  while (1) {
     int pid = fork();
     if (pid < 0) {
-        printf("Could not initialize");
-        exit(-1);
+      printf("Could not initialize");
+      exit(-1);
     }
     else if (pid == 0) P0();
     else if (pid > 0) P1();
+    if (time == 10) break;
+  }
 }
