11cd9be8215f11dc2e3e5a6edc81a2ca3be187a0
Do Duc Minh <22010176@st.phenikaa-uni.edu.vn>
2024-01-04 22:31:38 +0700

    ed

--- hw/SetAndGet.c
+++ hw/SetAndGet.c
@@ -7,79 +7,38 @@
 
 int TestAndSet(int* val) {
     int temp = *val;
-    *val = 0;
-    // printf("%d ", val);
+    *val = 1;
     return temp;
 }
 
 const int n = 5;
-int dua[] = { 1,1,1,1,1 };
-
-// int lp[] = { 0,0,0,0,0 };
-int p[] = { 0,0,0,0,0 };
-
-int Wait(int i) {
-    if (dua[i] && dua[(i + 1) % n]) return TestAndSet(dua + i) && TestAndSet(dua + (i + 1) % n);
-    return 0;
-}
-
-char Print(int a) { return a ? '*' : ' '; }
-void Test() {
-
-    for (int i = 0; i < n; i++) p[i] = Wait(i);
-
-    printf("            1.(%d)             \n", p[0]);
-    printf("\n");
-
-    printf("      1.%d          2.%d       \n", dua[0], dua[1]);
-    printf("\n");
-
-    printf("5.(%d)                       2.(%d)\n", p[4], p[1]);
-    printf("\n");
-
-    printf("   5.%d                 3.%d   \n", dua[4], dua[2]);
-    printf("\n");
-
-    printf("      4.(%d)    4.%d    3.(%d)       \n", p[3], dua[3], p[2]);
-    printf("\n");
-
-    for (int i = 0; i < n;i++) if (p[i] == 1 && rand() % 2 == 0) {
-        dua[i] = 1, dua[(i + 1) % n] = 1;
-        p[i] = 0;
-    }
+int dua[] = { 1,1,1,1,1 };              // đũa
+int p[] = { 0,0,0,0,0 };                // Nhà triết gia
 
 
-    sleep(1);
-}
+/*
+!(1 && 1) = !1 = 0 -> ( 1, 1 ) * (dừng vòng lặp)
+!(1 && 0) = !0 = 1 -> ( 1, 1 )
+!(0 && 1) = !0 = 1 -> ( 1, 1 )
+!(0 && 0) = !0 = 1 -> ( 1, 1 )
+*/
 
+int Wait(int i) { return !(TestAndSet(dua + i) && TestAndSet(dua + (i + 1) % n)); }
+void DangerJob() { sleep(2); }
+void UpdateState(int i) { dua[i] = dua[(i + 1) % n] = 1; }
+void SaveJob() {}
 
 void P(int i) {
     while (1) {
-        // TestAndSet sẽ để biến là true mỗi khi được gọi và trả về giá trị của biến lúc chưa đổi.
-        // 0 || 0 = 0 -> (1, 1): 2 đũa ở bên ko có người dùng và để là 1 ý chỉ có người chuẩn bị dùng. 
-        // 0 || 1 = 1 -> (1, 1): có 1 đũa ở bên cạnh, một đũa nữa đang được dùng, điều kiện đang đúng nên là phải chờ.
-        // 1 || 0 = 1 -> (1, 1)
-        // 1 || 1 -> (1, 1): Vì cả 2 đũa đều đang được dùng rồi nên là phải chờ.
-        while (p[i]);
+        while (Wait(i));
+        DangerJob();
 
-        // sleep(1); // làm việc nguy hiểm
+        UpdateState(i);
 
-        dua[i] = dua[(i + 1) % n] = 1;
-
-        // sleep(1); // làm việc khác
+        SaveJob();
     }
 }
 
 int main() {
-    srand(time(NULL));
-    // lock = false;
-    for (int i = 0;; ++i) {
-        printf("%d\n", i);
-        Test();
-
-    }
-
-    // printf("%d\n", dua[0]);
-    // TestAndSet(&dua[0]);
-    // printf("%d\n", dua[0]);
+    for (int i = 0; i < n; i++) StartProcess(P(i));
 }
